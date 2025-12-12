e2894bbb20c35fe1f8c8a45717267a00e2bfc3cc
shathasultan <shathasultann9@gmail.com>
2025-12-12 16:26:29 +0000

    Add python class files 1-6

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,22 +6,43 @@ Module defines a class square with a private instance attribute size
 
 class Square:
     """
-    Class Square that defines a square by: (based on 2-square.py)
+    class Square that defines a square by: (based on 3-square.py)
     """
     def __init__(self, size=0):
+
+        self.__size = size
         """
         Initialize a square with size.
         """
-        if not isinstance(size, int):
+    @property
+    def size(self):
+        """
+        Retrieve the size to the square.
+        Returns:
+            int: the size to the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Set the size of the square.
+        Args:
+            value (int): The new size of the square.
+        Raises:
+            TypeError: If 'value' is not an integer.
+            ValueError: If 'value' is less than 0.
+        """
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
         """
         Public instance method.
         Returns:
-            int: the current square area.
+            int: The current square area
         """
         return self.__size * self.__size
